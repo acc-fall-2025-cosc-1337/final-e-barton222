297b957f4f94337b9196948c88dc3e2ca8d9804a
e-barton222 <ethan.barton@g.austincc.edu>
2025-12-14 05:16:23 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,7 @@
-//
+#include "die.h"
+#include <cstdlib>
+
+int Die::roll()
+{
+    return (rand() % sides) + 1;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,5 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
+
+TEST_CASE("Die rolls values from 1 to 6", "[Die]") {
+    Die die;
+
+    for (int i = 0; i < 10; i++) {
+        int value = die.roll();
+        REQUIRE(value >= 1);
+        REQUIRE(value <= 6);
+    }
+}
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
