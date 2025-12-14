620cc1b97b788c5685ee2bc82288a57b5f186285
e-barton222 <ethan.barton@g.austincc.edu>
2025-12-14 05:22:58 +0000

    Question 2

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
+
+TEST_CASE("Roll returns value from 2 to 12", "[Roll]") {
+    Die die1;
+    Die die2;
+    Roll roll(die1, die2);
+
+    for (int i = 0; i < 10; i++) {
+        roll.roll_dice();
+        int value = roll.roll_value();
+        REQUIRE(value >= 2);
+        REQUIRE(value <= 12);
+    }
+}
 
 TEST_CASE("Die rolls values from 1 to 6", "[Die]") {
     Die die;
