97f48f7528e1244f420d67b30ad24ec4fd5c9ccf
e-barton222 <ethan.barton@g.austincc.edu>
2025-12-14 05:35:47 +0000

    Question 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,51 @@
-int main() 
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "die.h"
+#include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
+
+int main()
 {
-	
-	return 0;
+    srand(time(0));
+
+    Die die1, die2;
+    Shooter shooter;
+    Roll* roll;
+
+    roll = shooter.throw_dice(die1, die2);
+    int rolled_value = roll->roll_value();
+
+    ComeOutPhase come_out_phase;
+
+    while (come_out_phase.get_outcome(roll) == RollOutcome::natural ||
+           come_out_phase.get_outcome(roll) == RollOutcome::craps)
+    {
+        std::cout << "Rolled " << rolled_value << " roll again\n";
+        roll = shooter.throw_dice(die1, die2);
+        rolled_value = roll->roll_value();
+    }
+
+    std::cout << "Rolled " << rolled_value << " start of point phase\n";
+    std::cout << "Roll until " << rolled_value << " or a 7 is rolled\n";
+
+    int point = rolled_value;
+    roll = shooter.throw_dice(die1, die2);
+    rolled_value = roll->roll_value();
+
+    PointPhase point_phase(point);
+
+    while (point_phase.get_outcome(roll) == RollOutcome::nopoint)
+    {
+        std::cout << "Rolled " << rolled_value << " roll again\n";
+        roll = shooter.throw_dice(die1, die2);
+        rolled_value = roll->roll_value();
+    }
+
+    std::cout << "Rolled " << rolled_value << " end of point phase\n";
+
+    shooter.display_rolled_values();
+
+    return 0;
 }
